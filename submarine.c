77051bf935243d54c75859a3617aaccab8837a75
Ariel Jaro <arieljaro@gmail.com>
2015-11-11 15:51:01 +0200

    trying to help reut

--- submarine.c
+++ submarine.c
@@ -1,3 +1,4 @@
+//COMMENT
 //Author: Adi Mashiah, ID:305205676\Reut Lev, ID:305259186
 //Belongs to project: sysprog_submarine
 //This module represents the submarine entity. It recieves the radar image and decides about the output command.
